a91c5a4c9d037340cae852d583139628454ab760
Levvw <69399787+Levvw@users.noreply.github.com>
2020-10-31 23:08:30 +0200

    Update FindSquareRoot.cpp

--- code/tutorial-2/FindSquareRoot.cpp
+++ code/tutorial-2/FindSquareRoot.cpp
@@ -6,10 +6,11 @@ int main()
 {
     double num;
     cout << "Enter 5 positive numbers seperated by spaces \n";
-    cin >> num;
+  
     
     for (int count = 0; count <= 5; count++)
     {
+      cin >> num;
         if (num >= 0.0)
         {
             cout << "The square root of " << num << " is " << sqrt(num) << "\n";
